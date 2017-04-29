352ffd45e7d55f2b9d4db0942a49b011e999a4db
Mattiasgrondahl <mattias_grondahl@hotmail.com>
2017-04-29 12:16:38 +0200

    Update ADattributedump.ps1

--- ADattributedump.ps1
+++ ADattributedump.ps1
@@ -1,11 +1,1 @@
-<#.SYNOPSIS    ..DESCRIPTION    This script will find user in your    domain and will display their extended properties.
-.EXAMPLE    C:\PS>     .NOTES    Author: Mattias Gröndahl    Date  : April 5, 2017   #>
-Import-Module activedirectoryClear-Host
-$Properties = @('DisplayName','SamAccountName','LastLogonDate','Enabled','AccountExpirationDate','PasswordLastSet','EmailAddress','description''LastLogonTimestamp')
-#$date = 131258506728410260#function formatdate {#   [datetime]::FromFileTime($date).ToString('d MMMM yyy')#   write-host "test"#   }#formatdate
-#get users and attributes$Users = Get-ADUser -Filter * -SearchBase "Ou=Bjornen,dc=bjornen,dc=sfv,dc=se" -Properties $Properties | sort -Property LastLogonDate -Descending
-#Get enabled Aduser and the last login time$Last_users = Get-Aduser -Filter {Enabled -eq $true} -Properties Name, LastLogon | Select-Object Name, @{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}} | sort -Property LastLogon -Descending$i = 0$count = 0
-foreach($User in $Last_users)        {            try            {#                Write-host "Getting LastLogonTime for user: " $User ´n#                Get-Aduser $user -Properties Name, LastLogon | Select-Object Name, @{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}}                                  $count = $i++                            }            catch             {                write-host "error"            }
-            finally            {                #Write-host "Done proccessing user"            }
-}
-Write-Host "Here follows a list of last logged in users:" ´n sleep 2$Last_usersWrite-host Number of users = $count
+asd
