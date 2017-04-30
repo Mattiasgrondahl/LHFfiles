2a0533749d01927352553d888cfc00c7549d5375
Mattiasgrondahl <mattias_grondahl@hotmail.com>
2017-04-30 15:19:15 +0200

    Update locatefiles.ps1

--- locatefiles.ps1
+++ locatefiles.ps1
@@ -1,51 +1,62 @@
+<#
 
+.SYNOPSIS
+Find specific conent in TEXT files
+
+.DESCRIPTION    
+
+Performs the following task
+    - Get CSV of files
+    - Seach through TXT files for content
+    - Export content to text file
+            
+.NOTES    
+
+Requires Findfiles.ps1 script to be run prior to this script.
+
+        Author: Mattias Gröndahl    Date  : April 30, 2017   
+
+Checks the following.
+    -
+
+.EXAMPLE    
+
+.\FindContent.ps1 
+
+#>
+
+### Parameters
 $path = pwd
-$searchpath = Read-Host "Skriv path att söka på ex H:\"
+$listoffiles = Get-Content -Path "$path\output\txt_files.csv"
+$output_file = "$path\GDPR_all.txt"
 
-#Get-ChildItem -Path U:\ -Filter *password* -Recurse | Out-GridView
-#Get-ChildItem -Path U:\ -Filter *password* -Recurse | Where-Object { $_.Attributes
+$regex = '\b[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\[A-Za-z]{2,4}\b'
+$url_regex = '([a-zA-Z]{3,})://([\w-]+\.)+[\w-]+([\w-./?%&=]*)*?'
+$ip_regex = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+#$personnr = '\b\d{6,8}\-?\d{4}\b'
+$personnr = '\b\d{2}\[0-1]{1}\d{1}\[0-3]{1}\d{1}-?\d{4}\b'
+$Password = ''
 
-$result = Get-ChildItem -Filter *s* -Recurse | Where-Object {$_.Attributes -eq "Archive, Compressed"}
-
-$msg = "`nScript will search for the following file types: `r`n $files `r`n in the path $searchpath `n"
-if (Test-Path "$path\Extentions.txt") {    
-Write-host "File exsists"    
-$files = Get-Content -Path "$path\Extentions.txt"        
-Write-host "$msg"        
-sleep 1 
-}    
-Else {    
-Write-host "File doesn't exsists creating default file"        
+###
 
-New-Item Output\Extentions.txt -type file   
-Add-content Extentions.txt "txt`r`nbak`r`ndoc`r`ndocx`r`none`r`nkey`r`nvhd`r`nsh`r`nbat`r`ncmd`r`nps1`r`nrdg"    
-}
 
-$extentions = Get-Content -Path "Extentions.txt" 
-if (Test-Path "$path\Output") {    
-sleep 1   
-Write-Host "folder Exsists"    }    
-else {    
-Write-Host "Creating Folder"   
-New-Item Output -type Directory   
-sleep 1    }
-
-ForEach ($line in $extentions){   
+ForEach ($line in $listoffiles){    
 Write-Host $line    
-$content = Get-ChildItem -Path $searchpath -Filter *.$line -Recurse     
-if ($content -ne $null)    {       
-write-host "Found $line files, exporting to csv"       
-$content.VersionInfo |select Filename | Export-Csv -Path $path\output\$line"_files".csv       
-sleep 1    }    
-else {    
-Write-host "Found no $line files."    
-sleep 1    }    }
-$output_list = Get-ChildItem $path\Output$output_list
+#trim UNC path    
+$input_path = $line -replace '["]',''    
 
-#ToDO#Filter README.txt and others
+#Find Ip
+$search_ip = select-string -Path $input_path -Pattern $ip_regex -AllMatches | % { $_.Matches } | % { $_.Value} 
+Add-Content  $path\Output\ip.txt $search_ip
+#Find URL
+$search_url = select-string -Path $input_path -Pattern $url_regex -AllMatches | % { $_.Matches } | % { $_.Value} 
+Add-Content  $path\Output\url.txt $search_url
 
-#Later#Get-Content to read in files for each file
-#one file#$file = Get-ChildItem H:\servers.txt#$file.VersionInfo |select Filename
-#show line number#Select-String .\servers.txt -Pattern brons |select LineNumber, Line |fl
-#Search registry #ls 'HKLM:\SOFTWARE\Microsft\' -Recurse |findstr Version
-
+#Find Personnr
+$search_personnr = select-string -Path $input_path -Pattern $personnr -AllMatches | % { $_.Matches } | % { $_.Value} 
+Add-Content  $path\Output\personnr.txt $search_personnr
+#Find Password
+$search_password = select-string -Path $input_path -Pattern "password", "lösenord", "pinkod"
+Add-Content  $path\Output\passwords.txt $search_password#
+$search_password > $path\Output\passwords.txt
+}
