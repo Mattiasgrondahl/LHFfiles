eea391681951ab8c28b01205550649095604668a
Mattiasgrondahl <mattias_grondahl@hotmail.com>
2017-05-01 09:51:16 +0200

    Update FindFiles.ps1

--- FindFiles.ps1
+++ FindFiles.ps1
@@ -1,72 +1,51 @@
-<#
 
-.SYNOPSIS
-Finds ActiveDirecotry Users and their attributes
+$path = pwd
+$searchpath = Read-Host "Skriv path att söka på ex H:\"
 
-.DESCRIPTION    
+#Get-ChildItem -Path U:\ -Filter *password* -Recurse | Out-GridView
+#Get-ChildItem -Path U:\ -Filter *password* -Recurse | Where-Object { $_.Attributes
 
-Performs the following task
-    - Get AD users
-    - List attbiutes
-    - Export to CSV
-    - Displays output
-        
-.NOTES    
+$result = Get-ChildItem -Filter *s* -Recurse | Where-Object {$_.Attributes -eq "Archive, Compressed"}
+
+$msg = "`nScript will search for the following file types: `r`n $files `r`n in the path $searchpath `n"
+if (Test-Path "$path\Extentions.txt") {    
+Write-host "File exsists"    
+$files = Get-Content -Path "$path\Extentions.txt"        
+Write-host "$msg"        
+sleep 1 
+}    
+Else {    
+Write-host "File doesn't exsists creating default file"        
 
-Requires connection to the AD
-
-RSAT tools need to be installed for ActiveDirectory module to be used.
-
-        Author: Mattias Gröndahl    Date  : April 5, 2017   
-
-Checks the following.
-    -Internet Connection
-    -Is RSAT installed?
-
-.EXAMPLE    
-
-ADAttributedump -Test
-ADAttributedump -Verbose
-
-#>
-
-#Check for RSAT tools
-
-$HOME
-$d1 = 'WindowsTH-KB2693643-x64.msu'
-$Destination =  Join-Path -Path $HOME -ChildPath "Downloads\$dl"
-$URL = "https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A-8104-6A71FF9D98AB/" + $d1
-
- If (Get-HotFix -Id KB2693643 -ErrorAction SilentlyContinue) {
-
-        Write-Verbose '---RSAT for Windows 10 is already installed'
-        Write-Host '---RSAT for Windows 10 is already installed'
-
-    } Else {
-
-        Write-Verbose '---Downloading RSAT for Windows 10'
-        Write-Host '---Downloading RSAT for Windows 10'
-        $WebClient = New-Object System.Net.WebClient
-        $WebClient.DownloadFile($URL, "$Destination")
-        $WebClient.Dispose()
-        wusa.exe $Destination /quiet /norestart /log:$home\Documents\RSAT.log
-
-        Enable-WindowsOptionalFeature -Online -FeatureName RSATClient-Roles-AD-Powershell
-    }
-
-
-Import-Module activedirectory
-Clear-Host
-
-$Properties = @('DisplayName','SamAccountName','LastLogonDate','Enabled','AccountExpirationDate','PasswordLastSet','EmailAddress','description''LastLogonTimestamp')
-
-#$date = 131258506728410260#function formatdate {#   [datetime]::FromFileTime($date).ToString('d MMMM yyy')#   write-host "test"#   }#formatdate
-#get users and attributes$Users = Get-ADUser -Filter * -SearchBase "Ou=test,DC=test,OU=local" -Properties $Properties | sort -Property LastLogonDate -Descending
-#Get enabled Aduser and the last login time$Last_users = Get-Aduser -Filter {Enabled -eq $true} -Properties Name, LastLogon | Select-Object Name, @{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}} | sort -Property LastLogon -Descending$i = 0$count = 0
-
-foreach($User in $Last_users)        {            try            {#                Write-host "Getting LastLogonTime for user: " $User ´n#                Get-Aduser $user -Properties Name, LastLogon | Select-Object Name, @{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}}                                  $count = $i++                            }            catch             {                write-host "error"            }
-            finally            {                #Write-host "Done proccessing user"            }
+New-Item Output\Extentions.txt -type file   
+Add-content Extentions.txt "txt`r`nbak`r`ndoc`r`ndocx`r`none`r`nkey`r`nvhd`r`nsh`r`nbat`r`ncmd`r`nps1`r`nrdg"    
 }
 
-Write-Host "Here follows a list of last logged in users:" ´n sleep 2$Last_usersWrite-host Number of users = $count
+$extentions = Get-Content -Path "Extentions.txt" 
+if (Test-Path "$path\Output") {    
+sleep 1   
+Write-Host "folder Exsists"    }    
+else {    
+Write-Host "Creating Folder"   
+New-Item Output -type Directory   
+sleep 1    }
+
+ForEach ($line in $extentions){   
+Write-Host $line    
+$content = Get-ChildItem -Path $searchpath -Filter *.$line -Recurse     
+if ($content -ne $null)    {       
+write-host "Found $line files, exporting to csv"       
+$content.VersionInfo |select Filename | Export-Csv -Path $path\output\$line"_files".csv       
+sleep 1    }    
+else {    
+Write-host "Found no $line files."    
+sleep 1    }    }
+$output_list = Get-ChildItem $path\Output$output_list
+
+#ToDO#Filter README.txt and others
+
+#Later#Get-Content to read in files for each file
+#one file#$file = Get-ChildItem H:\servers.txt#$file.VersionInfo |select Filename
+#show line number#Select-String .\servers.txt -Pattern brons |select LineNumber, Line |fl
+#Search registry #ls 'HKLM:\SOFTWARE\Microsft\' -Recurse |findstr Version
 
