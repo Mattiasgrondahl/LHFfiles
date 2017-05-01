b6cfad53c2985b7ebda9f420cc2070c3046a8819
Mattiasgrondahl <mattias_grondahl@hotmail.com>
2017-05-01 09:50:33 +0200

    Update FindFiles.ps1

--- FindFiles.ps1
+++ FindFiles.ps1
@@ -1,62 +1,72 @@
 <#
 
 .SYNOPSIS
-Find specific conent in TEXT files
+Finds ActiveDirecotry Users and their attributes
 
 .DESCRIPTION    
 
 Performs the following task
-    - Get CSV of files
-    - Seach through TXT files for content
-    - Export content to text file
-            
+    - Get AD users
+    - List attbiutes
+    - Export to CSV
+    - Displays output
+        
 .NOTES    
 
-Requires Findfiles.ps1 script to be run prior to this script.
+Requires connection to the AD
 
-        Author: Mattias Gröndahl    Date  : April 30, 2017   
+RSAT tools need to be installed for ActiveDirectory module to be used.
+
+        Author: Mattias Gröndahl    Date  : April 5, 2017   
 
 Checks the following.
-    -
+    -Internet Connection
+    -Is RSAT installed?
 
 .EXAMPLE    
 
-.\FindContent.ps1 
+ADAttributedump -Test
+ADAttributedump -Verbose
 
 #>
 
-### Parameters
-$path = pwd
-$listoffiles = Get-Content -Path "$path\output\txt_files.csv"
-$output_file = "$path\GDPR_all.txt"
+#Check for RSAT tools
 
-$regex = '\b[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\[A-Za-z]{2,4}\b'
-$url_regex = '([a-zA-Z]{3,})://([\w-]+\.)+[\w-]+([\w-./?%&=]*)*?'
-$ip_regex = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
-#$personnr = '\b\d{6,8}\-?\d{4}\b'
-$personnr = '\b\d{2}\[0-1]{1}\d{1}\[0-3]{1}\d{1}-?\d{4}\b'
-$Password = ''
+$HOME
+$d1 = 'WindowsTH-KB2693643-x64.msu'
+$Destination =  Join-Path -Path $HOME -ChildPath "Downloads\$dl"
+$URL = "https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A-8104-6A71FF9D98AB/" + $d1
 
-###
+ If (Get-HotFix -Id KB2693643 -ErrorAction SilentlyContinue) {
+
+        Write-Verbose '---RSAT for Windows 10 is already installed'
+        Write-Host '---RSAT for Windows 10 is already installed'
+
+    } Else {
+
+        Write-Verbose '---Downloading RSAT for Windows 10'
+        Write-Host '---Downloading RSAT for Windows 10'
+        $WebClient = New-Object System.Net.WebClient
+        $WebClient.DownloadFile($URL, "$Destination")
+        $WebClient.Dispose()
+        wusa.exe $Destination /quiet /norestart /log:$home\Documents\RSAT.log
+
+        Enable-WindowsOptionalFeature -Online -FeatureName RSATClient-Roles-AD-Powershell
+    }
 
 
-ForEach ($line in $listoffiles){    
-Write-Host $line    
-#trim UNC path    
-$input_path = $line -replace '["]',''    
+Import-Module activedirectory
+Clear-Host
 
-#Find Ip
-$search_ip = select-string -Path $input_path -Pattern $ip_regex -AllMatches | % { $_.Matches } | % { $_.Value} 
-Add-Content  $path\Output\ip.txt $search_ip
-#Find URL
-$search_url = select-string -Path $input_path -Pattern $url_regex -AllMatches | % { $_.Matches } | % { $_.Value} 
-Add-Content  $path\Output\url.txt $search_url
+$Properties = @('DisplayName','SamAccountName','LastLogonDate','Enabled','AccountExpirationDate','PasswordLastSet','EmailAddress','description''LastLogonTimestamp')
 
-#Find Personnr
-$search_personnr = select-string -Path $input_path -Pattern $personnr -AllMatches | % { $_.Matches } | % { $_.Value} 
-Add-Content  $path\Output\personnr.txt $search_personnr
-#Find Password
-$search_password = select-string -Path $input_path -Pattern "password", "lösenord", "pinkod"
-Add-Content  $path\Output\passwords.txt $search_password#
-$search_password > $path\Output\passwords.txt
+#$date = 131258506728410260#function formatdate {#   [datetime]::FromFileTime($date).ToString('d MMMM yyy')#   write-host "test"#   }#formatdate
+#get users and attributes$Users = Get-ADUser -Filter * -SearchBase "Ou=test,DC=test,OU=local" -Properties $Properties | sort -Property LastLogonDate -Descending
+#Get enabled Aduser and the last login time$Last_users = Get-Aduser -Filter {Enabled -eq $true} -Properties Name, LastLogon | Select-Object Name, @{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}} | sort -Property LastLogon -Descending$i = 0$count = 0
+
+foreach($User in $Last_users)        {            try            {#                Write-host "Getting LastLogonTime for user: " $User ´n#                Get-Aduser $user -Properties Name, LastLogon | Select-Object Name, @{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}}                                  $count = $i++                            }            catch             {                write-host "error"            }
+            finally            {                #Write-host "Done proccessing user"            }
 }
+
+Write-Host "Here follows a list of last logged in users:" ´n sleep 2$Last_usersWrite-host Number of users = $count
+
